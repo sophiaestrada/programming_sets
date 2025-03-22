4962b1a30f6db770de99ef57b494a585e1986564
sophiaestrada <sophia.estrada@student.ateneo.edu>
2025-03-22 19:33:25 +0800

    Update set_2.py

--- set_2.py
+++ set_2.py
@@ -1,1 +1,18 @@
+# no 1
+def shift_letter(letter, shift):
+  if letter == " ":
+        return " "
+  return chr((ord(letter) - ord("A") + shift) % 26 + ord("A"))
 
+
+# no 2
+def caesar_cipher(message, shift):
+  output = ""
+
+  for letter in message:
+    if letter == " ":
+        output += " "
+    else:
+        output += chr((ord(letter) - ord("A") + shift) % 26 + ord("A"))
+  
+  return output
